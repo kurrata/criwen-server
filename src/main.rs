90eadf556d4628f98f5167bb8329bc1278c28525
Mārtiņš Briedis <martins.briedis@mabri.org>
2021-06-21 00:20:43 +0300

    Add logging level argument to cli

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use log;
 use simple_logger::SimpleLogger;
-use clap::{App, SubCommand, ArgMatches};
+use clap::{App, SubCommand, ArgMatches, Arg};
 use crate::server::GameServer;
+use log::{Level, LevelFilter};
 
 mod net;
 mod server;
@@ -10,32 +11,51 @@ mod core;
 
 #[tokio::main]
 async fn main() {
-    SimpleLogger::new().init().unwrap();
     let mut app = App::new("Criwen")
-        .about("Criwen game server")
+        .args(&[
+            Arg::with_name("v").short("v").multiple(true).help("Sets the level of verbosity\
+                \n\t0 - Error\
+                \n\t1 - Warn\
+                \n\t2 - Info\
+                \n\t3 - Debug\
+                \n\t4 - Trace\
+                "),
+        ])
         .subcommand(SubCommand::with_name("start")
-            .about("Start game server")
+            .about("Start server")
         )
         .subcommand(SubCommand::with_name("stop")
-            .about("Stop game server")
+            .about("Stop server")
         )
         .subcommand(SubCommand::with_name("restart")
-            .about("Restart game server")
+            .about("Restart server")
         )
         .subcommand(SubCommand::with_name("ui")
             .about("Run administration UI")
         );
 
+    init_logging(app.clone().get_matches().occurrences_of("v")).await;
 
-    match app.clone().get_matches().subcommand().clone() {
+    match app.clone().get_matches().subcommand() {
         ("start", Some(sub_app)) => { start_server().await }
         ("stop", Some(sub_app)) => {}
         ("restart", Some(sub_app)) => {}
         ("ui", Some(sub_app)) => {}
-        _ => {
+        (a, b) => {
             app.print_help().unwrap();
         }
     }
+}
+
+async fn init_logging(level: u64) {
+    let mut logger = SimpleLogger::new();
+    match level {
+        0 => { logger.with_level(LevelFilter::Error) }
+        1 => { logger.with_level(LevelFilter::Warn) }
+        2 => { logger.with_level(LevelFilter::Info) }
+        3 => { logger.with_level(LevelFilter::Debug) }
+        _ => { logger.with_level(LevelFilter::Trace) }
+    }.init().unwrap();
 }
 
 async fn start_server() {
