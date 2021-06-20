c53e3a70a708d24228c6c774710ef5e6ea4b68f1
Mārtiņš Briedis <martins.briedis@mabri.org>
2021-06-20 22:36:29 +0300

    Add cli interface to start server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use log;
 use simple_logger::SimpleLogger;
 use crate::game::{Criwen};
+use clap::{App, SubCommand, ArgMatches};
 
 mod game;
 mod net;
@@ -9,8 +10,34 @@ mod net;
 #[tokio::main]
 async fn main() {
     SimpleLogger::new().init().unwrap();
-    log::info!("Server starting");
-    let criwen = Criwen::new();
-    Criwen::init(criwen).await;
-    log::info!("Server shut down");
+    let mut app = App::new("Criwen")
+        .about("Criwen game server")
+        .subcommand(SubCommand::with_name("start")
+            .about("Start game server")
+        )
+        .subcommand(SubCommand::with_name("stop")
+            .about("Stop game server")
+        )
+        .subcommand(SubCommand::with_name("restart")
+            .about("Restart game server")
+        )
+        .subcommand(SubCommand::with_name("ui")
+            .about("Run administration UI")
+        );
+
+
+    match app.clone().get_matches().subcommand().clone() {
+        ("start", Some(sub_app)) => {
+            log::info!("Server starting");
+            let criwen = Criwen::new();
+            Criwen::init(criwen).await;
+            log::info!("Server shut down");
+        }
+        ("stop", Some(sub_app)) => {}
+        ("restart", Some(sub_app)) => {}
+        ("ui", Some(sub_app)) => {}
+        _ => {
+            app.print_help().unwrap();
+        }
+    }
 }
